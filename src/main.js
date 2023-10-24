5963658bc7a3559c4e7889ba3883991353d20ccf
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-24 11:39:47 +0200

    we have the components

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import './style.css'
 import axios from 'axios';
 import { Employees } from './components/Employees';
+import { Customers } from './components/Customers';
 
-const url = 'https://edwardtanguay.vercel.app/share/employees.json';
+const employeesUrl = 'https://edwardtanguay.vercel.app/share/employees.json';
+const customersUrl = 'https://edwardtanguay.vercel.app/share/customers.json';
 
-const employees = (await axios.get(url)).data;
+const employees = (await axios.get(employeesUrl)).data;
+const customers = (await axios.get(customersUrl)).data;
 
 document.querySelector('#app').innerHTML = `
   <h1>Axios example</h1>
   ${Employees(employees)}
+  ${Customers(customers)}
 `
