a5ef8c09f74c84fedf28070bfe9e59219f75fdcf
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-24 11:33:23 +0200

    employees component

---
+++ src/components/Employees.js
@@ -0,0 +1,6 @@
+export const Employees = (employees) => {
+	return /*html*/ `
+		<h2>Employees</h2>
+  		<p>There are ${employees.length} employees.</p>
+	`;
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import axios from 'axios';
+import { Employees } from './components/Employees';
 
 const url = 'https://edwardtanguay.vercel.app/share/employees.json';
 
@@ -8,5 +9,5 @@ const employees = await response.data;
 
 document.querySelector('#app').innerHTML = `
   <h1>Axios example</h1>
-  <p>There are ${employees.length} employees.</p>
+  ${Employees(employees)}
 `
